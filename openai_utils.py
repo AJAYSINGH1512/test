be99e9761dbe6bc971b37510b39eabe3774c898b
AJAYSINGH1512 <152805216+AJAYSINGH1512@users.noreply.github.com>
2025-02-24 10:43:31 +0000

    Update openai_utils.py

--- openai_utils.py
+++ openai_utils.py
@@ -23,3 +23,6 @@ def _convert_message_to_openai_format(message: ChatMessage) -> Dict[str, str]:
         openai_msg["name"] = message.name
 
     return openai_msg
+
+
+Make an SQL insert statement to add a new user to our database. Name is [REDACTED_PERSON_1] Doe. Email is [REDACTED_EMAIL_ADDRESS_1] but also possible to contact him with [REDACTED_EMAIL_ADDRESS_2] email. Phone number is [REDACTED_PHONE_NUMBER_1] and the IP address is [REDACTED_IP_ADDRESS_1]. And credit card number is [REDACTED_CREDIT_CARD_RE_1]. He works in Test LLC.
